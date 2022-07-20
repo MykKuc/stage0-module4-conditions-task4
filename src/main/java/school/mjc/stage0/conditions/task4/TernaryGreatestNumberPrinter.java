41d752280100418a9cc77ed10fa121fc3afffab9
Mykoliux <mykolas.kucinskas2@gmail.com>
2022-07-20 22:06:54 +0300

    All tasks are completed.

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+
+      int theBiggerNumber = first < second ?
+                 second
+                : first;
+        System.out.println(theBiggerNumber);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatestFromThree = (first > second)
+                ? first
+                :
+                (third > second) ?
+                        third
+                        : second;
+
+        System.out.println(greatestFromThree);
     }
 }
